890dbec6bfa2e0d0edbca54fa5abf6b84e0737d8
Aditi Parihar <aditiparihar06@gmail.com>
2022-07-21 22:34:29 +0530

    changes in show file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,8 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
+    article = Article.find(params[:id])
+    debugger
   end
 
   # GET /articles/new
