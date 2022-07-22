cd5f57a9af1c75e6f7d8c3b4cf3a13790ffbf528
Aditi Parihar <aditiparihar06@gmail.com>
2022-07-22 21:28:47 +0530

    add changes in views folder

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,6 @@ class ArticlesController < ApplicationController
   # GET /articles/1 or /articles/1.json
   def show
     article = Article.find(params[:id])
-    debugger
   end
 
   # GET /articles/new
